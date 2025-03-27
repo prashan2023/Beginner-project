4b9bc0784d69aea313c4799ce5824fd892ffc6a3
PrasaK <prashanramachandran123@gamil.com>
2025-03-27 17:38:05 +0530

    counter project

---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Counter = () => {
+  const[count,setCount] = useState(0);
+  const increment =()=>{
+    setCount(count + 1)
+  };
+  const decrement =()=>{
+    setCount(count-1)
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="count">{count}</h1>
+      </div>
+      
+      <div className="btn-container">
+        <button onClick={decrement} className="decrement"> - </button> 
+        <button onClick={increment} className="increment"> + </button>   
+      </div>
+    </div>
+  )
+}
+
+export default Counter;
