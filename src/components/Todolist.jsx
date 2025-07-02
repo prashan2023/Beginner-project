29a6c8f52c2f1ef23b54e149e00a23b986a96fc6
PrasaK <prashanramachandran123@gamil.com>
2025-07-02 13:15:52 +0530

    add todo icon

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Todolist = () => {
   return (
     <div>
-      <h1>Welcome to the todo application</h1>
+      <h1>Welcome to the todo application 🈸</h1>
     </div>
   )
 }
